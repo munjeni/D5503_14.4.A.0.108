4fade70b83aa50d17b5681e46ce02147194497c2
munjeni <sharecard17@gmail.com>
2014-09-04 19:59:00 +0200

    smartass v2 and lagfree governors

--- system/etc/install-recovery.sh
+++ system/etc/install-recovery.sh
@@ -1,4 +1,5 @@
 #!/system/bin/sh
 /system/xbin/su --daemon &
 insmod /system/lib/modules/cpufreq_lionheart.ko
-
+insmod /system/lib/modules/cpufreq_smartass2.ko
+insmod /system/lib/modules/cpufreq_lagfree.ko
