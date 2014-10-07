b760bf3df4b399e2f94cead601b1751240643193
munjeni <sharecard17@gmail.com>
2014-10-07 16:48:55 +0200

    revert commit: smartass v2 and lagfree governors Its not working good.

--- system/etc/install-recovery.sh
+++ system/etc/install-recovery.sh
@@ -1,5 +1,3 @@
 #!/system/bin/sh
 /system/xbin/su --daemon &
 insmod /system/lib/modules/cpufreq_lionheart.ko
-insmod /system/lib/modules/cpufreq_smartass2.ko
-insmod /system/lib/modules/cpufreq_lagfree.ko
